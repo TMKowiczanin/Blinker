3f90c1bef6cd6845f9e25ec3bd6a7c6fb6bed360
TMKii <Tomasz.moron@polsl.pl>
2019-10-17 23:20:52 +0200

    Teraz migają kolorowe soczki i piwo trzeba dodać jeszcze zapis sekwencji i zapis czasu uruchomienia akcji

--- Migacz/Migacz/ImageBlinker.cs
+++ Migacz/Migacz/ImageBlinker.cs
@@ -13,25 +13,42 @@ namespace Migacz
     public partial class ImageBlinker : Form
     {
         private int secPP;
-        private Image Target=    Image.FromFile("imgs\\target.jpg");
-        private Image Red =      Image.FromFile("imgs\\red.jpg");
+        private Image Target    =Image.FromFile("imgs\\target.jpg");
+        private Image Red       =Image.FromFile("imgs\\red.jpg");
+        private Image Green     =Image.FromFile("imgs\\green.jpg");
+        private Image Orange    =Image.FromFile("imgs\\orange.jpg");
+        private Random rnd = new Random();
+        private int licznik = 0;
         //private Image Green =    Image.FromFile("imgs\green.jpg");
         //private Image Orange =   Image.FromFile("imgs\orange.jpg");
         Rectangle resolution = Screen.PrimaryScreen.Bounds;
         public ImageBlinker()
         {
             InitializeComponent();
+             //CreateText(string path);
+            //System.IO.StreamWriter writer = new System.IO.StreamWriter("wyniki\\ble.txt"); //open the file for writing.
+            //writer.Write(DateTime.Now.ToString()); //write the current date to the file. change this with your date or something.
+            //writer.Close(); //remember to close the file again.
+            //writer.Dispose(); //remember to dispose it from the memory.
             this.secPP = 500;
             
-
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Maximized;
             TopMost = true;
             timerImage.Interval = this.secPP;
             timerImage.Start();
-            BackColor = Color.White;
+            BackColor = Color.Black;
         }
+        private void SetImage(Image IMG)
+        {
+            
+            pictureBox1.Width = IMG.Width;
+            pictureBox1.Height = IMG.Height;
+            pictureBox1.Image = IMG;
+            pictureBox1.Location = new Point((resolution.Width / 2) - (pictureBox1.Width / 2), (resolution.Height / 2) - (pictureBox1.Height / 2));
 
+
+        }
         private void ImageBlinker_Load(object sender, EventArgs e)
         {
 
@@ -53,16 +70,28 @@ namespace Migacz
 
         private void timerImage_Tick(object sender, EventArgs e)
         {
+            if (this.licznik== 300*2)
+                this.Close();
+
+
+            this.licznik++;
             if (BackColor == Color.White)
             {
-                BackColor = Color.Black;
+                BackColor = Color.Gray;
                 pictureBox1.Hide();
             }
 
             else
             {
                 BackColor = Color.White;
-                pictureBox1.Image = this.Target;
+                if(rnd.Next(1,5)==1)
+                SetImage(Target);
+                if (rnd.Next(1, 5) == 2)
+                    SetImage(Red);
+                if (rnd.Next(1, 5) == 3)
+                    SetImage(Orange);
+                if (rnd.Next(1, 5) == 4)
+                    SetImage(Green);
                 pictureBox1.Show();
             }
         }
